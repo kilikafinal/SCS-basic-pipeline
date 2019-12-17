b37cde11bcda0a03cb22e2004e7a655ee047236f
Javin <kilikafinal@gmail.com>
2019-12-16 22:18:46 -0800

    initial commit

---
+++ src/main/java/com/personal/SCSpipeline/ScsPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.personal.SCSpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScsPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScsPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/SCSpipeline/ScsPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.SCSpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScsPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
